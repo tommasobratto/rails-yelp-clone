cd581a7483e0cd0591ca9dd5f028e0cbbe06373d
tommasobratto <tbratto22@gmail.com>
2015-03-31 17:42:06 +0100

    added sign in through OmniAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {:omniauth_callbacks => "user/omniauth_callbacks"}
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -61,4 +62,7 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
+  # devise_scope :user do 
+  #   get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  # end
 end
